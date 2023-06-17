c5575fa3b652200c9b5467a64c4b48085aa3c0b0
class1 <class@gmail.com>
2023-06-17 19:03:42 +0530

    Files are commited

---
+++ app.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AdmissionForm from './components/forms/AdmissionForm';
+
+function App() {
+  return (
+    <div className="App">
+      <AdmissionForm/>
+    </div>
+  );
+}
+
+export default AdmissionForm;
---
+++ header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function AdmissionForm() {
+    return(
+        <nav class="bg-dark navbar-dark navbar">
+            <div className="row col-12 d-flex justify-content-center text-white">
+                <h3>AdmissionForm</h3>
+            </div>
+        </nav>
+    )
+}
+export default AdmissionForm ;
